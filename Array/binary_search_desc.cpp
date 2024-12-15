7b26004b13b01cc331defee92fa3383fa518ce6d
shivam-singh20 <shivamks5689@gmail.com>
2024-12-15 14:56:57 +0530

    Add code

---
+++ Array/binary_search_desc.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int BinarySearch(int arr[], int n, int key)
+{
+    int start = 0;
+    int end = n - 1;
+    while (start <= end)
+    {
+        int mid = (start + end) / 2;
+
+        if (arr[mid] == key)
+        {
+            return mid;
+            break;
+        }
+        else if (arr[mid] < key)
+            end = mid - 1;
+        else
+            start = mid + 1;
+    }
+    return -1;
+}
+
+int main()
+{
+    int n, arr[1000], key;
+    cout << "Enter size of the array: ";
+    cin >> n;
+    cout << "Enter number in descending order:\n";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    cout << "Enter the element to search for: ";
+    cin >> key;
+
+    cout << "Number is present on " << BinarySearch(arr, n, key) << " index.";
+    return 0;
+}
