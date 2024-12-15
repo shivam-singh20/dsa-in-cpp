5d126708408b1ffe3f7ac80c8459ee1442ad2106
shivam-singh20 <shivamks5689@gmail.com>
2024-12-16 00:16:51 +0530

    Add code

---
+++ Array/square_root.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int Square_root(int num)
+{
+    int ans,mid,start=0,end=num;
+
+    while(start<=end){
+        mid=start+(end-start)/2;
+        if(mid*mid==num){
+            ans=mid;
+            break;
+        }
+        else if(mid*mid<num){
+            ans=mid;
+            start=mid+1;
+        }
+        else{
+            end=mid-1;
+        }
+    }
+    return ans;
+}
+
+
+int main (){
+    int num;
+    cout<<"Enter the number whose square root is to be found: ";
+    cin>>num;
+    cout<<"Square root of "<<num<<" is "<<Square_root(num);
+    return 0;
+}
