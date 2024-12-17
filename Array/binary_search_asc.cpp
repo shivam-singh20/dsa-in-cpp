ac68be17f409e662024d6df8d4880f451647247f
shivam-singh20 <shivamks5689@gmail.com>
2024-12-17 16:37:21 +0530

    Add code

--- Array/binary_search_asc.cpp
+++ Array/binary_search_asc.cpp
@@ -1,37 +1,44 @@
 #include <iostream>
 using namespace std;
 
-int BinarySearch(int a[],int n,int key){
-    int start=0;
-    int end=n-1;
-    while(start<=end){
-        int mid=(start+end)/2;
-        if(a[mid]==key){
+int BinarySearch(int a[], int n, int key)
+{
+    int start = 0;
+    int end = n - 1;
+    while (start <= end)
+    {
+        int mid = (start + end) / 2;
+        if (a[mid] == key)
+        {
             return mid;
             break;
         }
-        else if(a[mid]<key){
-            start=mid+1;
-        } 
-        else{
-            end=mid-1;
+        else if (a[mid] < key)
+        {
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid - 1;
         }
     }
     return -1;
 }
 
-int main (){
-    int n,arr[1000],key;
-    cout <<"Enter size of the array: ";
-    cin>>n;
-    cout<<"Enter number in ascending order:\n";
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
+int main()
+{
+    int n, arr[1000], key;
+    cout << "Enter size of the array: ";
+    cin >> n;
+    cout << "Enter number in ascending order:\n";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
     }
-    cout<<"Enter the element to search for: ";
-    cin>>key;
+    cout << "Enter the element to search for: ";
+    cin >> key;
 
-    cout<<"Number is present on "<<BinarySearch(arr,n,key)<<" index.";
+    cout << "Number is present on " << BinarySearch(arr, n, key) << " index.";
 
     return 0;
 }
