7411bf654f5a58a1cf8f040b69d83e3436f81624
shivam-singh20 <shivamks5689@gmail.com>
2024-12-18 19:57:25 +0530

    Add code

---
+++ Array/kth_missing_integer.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int missing_integer(int arr[],int n,int k){
+    int start=0,end=n-1,mid,ans=-1;
+    while(start<=end){
+        mid=start+(end-start)/2;
+        if(arr[mid]-mid-1>=k){
+            ans=mid;
+            end=mid-1;
+        }
+        else
+            start=mid+1;
+    }
+    return ans+=k;
+}
+
+int main (){
+    int arr[1000],n,k;
+    cout<<"Enter size of the array: ";
+    cin>>n;
+    cout<<"Enter elements of the array:\n";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<"Enter which kth number is to find: ";
+    cin>>k;
+
+    cout<<"Kth number is "<<missing_integer(arr,n,k);
+
+    return 0;
+}
