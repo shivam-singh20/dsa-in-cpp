c94a1ac6fee0c1a3a2588ea89815138570d82d04
shivam-singh20 <shivamks5689@gmail.com>
2024-12-21 20:56:25 +0530

    Add code

--- Important_Problems/Ship_Packages.cpp
+++ Important_Problems/Ship_Packages.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int Painter_Partition(int arr[], int n, int n_painters)
+int Ship_Packages(int arr[], int n, int n_days)
 {
     int start = 0, end = 0, mid, ans;
     for (int i = 0; i < n; i++)
@@ -12,17 +12,17 @@ int Painter_Partition(int arr[], int n, int n_painters)
     while (start <= end)
     {
         mid = start + (end - start) / 2;
-        int walls_length = 0, count = 1;
+        int packages_weights = 0, count = 1;
         for (int i = 0; i < n; i++)
         {
-            walls_length += arr[i];
-            if (walls_length > mid)
+            packages_weights += arr[i];
+            if (packages_weights > mid)
             {
                 count++;
-                walls_length = arr[i];
+                packages_weights = arr[i];
             }
         }
-        if (count <= n_painters)
+        if (count <= n_days)
         {
             ans = mid;
             end = mid - 1;
@@ -35,18 +35,18 @@ int Painter_Partition(int arr[], int n, int n_painters)
 
 int main()
 {
-    int arr[1000], n, n_painters;
-    cout << "Enter number of walls: ";
+    int arr[1000], n, n_days;
+    cout << "Enter number of packages: ";
     cin >> n;
-    cout << "Enter lengths of the walls:\n";
+    cout << "Enter weights of that packages:\n";
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
     }
-    cout << "Enter number of painters: ";
-    cin >> n_painters;
+    cout << "Enter number of days: ";
+    cin >> n_days;
 
-    cout << "Therefore minimum time required to paint the walls is " << Painter_Partition(arr, n, n_painters);
+    cout << "Therefore minimum days required to ship the packages are " << Ship_Packages(arr, n, n_days) << " days";
 
     return 0;
 }
