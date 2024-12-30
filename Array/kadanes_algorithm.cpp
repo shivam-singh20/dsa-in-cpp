fdc5e1017ed8ee27e865efbbb13774f1929e9cee
shivam-singh20 <shivamks5689@gmail.com>
2024-12-30 23:19:28 +0530

    Add code

--- Array/kadanes_algorithm.cpp
+++ Array/kadanes_algorithm.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <climits>
 using namespace std;
 
 int main (){
-return 0;
+    int arr[1000], n;
+    cout << "Enter size of the array: ";
+    cin >> n;
+    cout << "Enter elements:\n";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int maxi=INT_MIN,prefix=0;
+    for(int i=0;i<n;i++){
+        prefix+=arr[i];
+        maxi=max(prefix,maxi);
+        if(prefix<0)
+            prefix=0;
+    }
+    cout<<"Maximum sum of subarray is: "<<maxi;
+    return 0;
 }
