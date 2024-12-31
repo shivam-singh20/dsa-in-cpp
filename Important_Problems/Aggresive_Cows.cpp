1ce8fda122cf40750865cfbbd6caa839be7b4a49
shivam-singh20 <shivamks5689@gmail.com>
2024-12-31 23:14:08 +0530

    Add code

--- Important_Problems/Aggresive_Cows.cpp
+++ Important_Problems/Aggresive_Cows.cpp
@@ -1,31 +1,34 @@
 #include <iostream>
 using namespace std;
 
-int Solution(int arr[],int stalls,int cows)
+int Solution(int arr[], int stalls, int cows)
 {
-    int start=arr[0],end=arr[stalls-1]-arr[0],mid,ans;
-    while(start<=end){
-        mid=start+(end-start)/2;
-        int count=1,position=arr[0];
-        for(int i=1;i<stalls;i++)
+    int start = arr[0], end = arr[stalls - 1] - arr[0], mid, ans;
+    while (start <= end)
+    {
+        mid = start + (end - start) / 2;
+        int count = 1, position = arr[0];
+        for (int i = 1; i < stalls; i++)
         {
-            if(position+mid<=arr[i]){
+            if (position + mid <= arr[i])
+            {
                 count++;
-                position=arr[stalls];
+                position = arr[stalls];
             }
-        }    
-        if(count>cows)
+        }
+        if (count > cows)
         {
-            ans=mid;
-            start=mid+1;
+            ans = mid;
+            start = mid + 1;
         }
         else
-            end=mid-1;
+            end = mid - 1;
     }
     return ans;
 }
 
-int main (){
+int main()
+{
     int arr[1000], stalls, cows;
     cout << "Enter number of stalls: ";
     cin >> stalls;
@@ -37,7 +40,7 @@ int main (){
     cout << "Enter number of cows: ";
     cin >> cows;
 
-    cout << "The minimum distance between cows is " << Solution(arr, stalls,cows);
+    cout << "The minimum distance between cows is " << Solution(arr, stalls, cows);
 
     return 0;
 }
