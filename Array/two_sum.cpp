15c0cf0b8c0a8c0eef88dd700fbececb2568720a
shivam-singh20 <shivamks5689@gmail.com>
2025-01-02 23:22:10 +0530

    Add code

--- Array/two_sum.cpp
+++ Array/two_sum.cpp
@@ -1,49 +1,56 @@
 #include <iostream>
-#include<algorithm>
+#include <algorithm>
 using namespace std;
 
-void On(int arr[],int n,int target){
-    //Array should be in sorted form
-    int start=0,end=n-1;
-    while(start<end){
-        if(arr[start]+arr[end]==target){
-            cout<<start<<" "<<end;
+void On(int arr[], int n, int target)
+{
+    // Array should be in sorted form
+    int start = 0, end = n - 1;
+    while (start < end)
+    {
+        if (arr[start] + arr[end] == target)
+        {
+            cout << start << " " << end;
             break;
         }
-        else if(arr[start]+arr[end]>target)
+        else if (arr[start] + arr[end] > target)
             end--;
         else
             start++;
     }
 }
 
-void On2(int arr[],int n,int target){
+void On2(int arr[], int n, int target)
+{
 
-    //For T.C O(n2)
-    cout<<"The numbers are: ";
-    for(int i=0;i<n-1;i++){
-        for(int j=i+1;j<n;j++){
-            if(arr[i]+arr[j]==target)
-                cout<<arr[i]<<" "<<arr[j];
+    // For T.C O(n2)
+    cout << "The numbers are: ";
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[i] + arr[j] == target)
+                cout << arr[i] << " " << arr[j];
         }
     }
 }
 
-int main(){
-    int arr[1000],n,target;
-    cout<<"Enter size of the array: ";
-    cin>>n;
-    cout<<"Enter elements:\n";
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
+int main()
+{
+    int arr[1000], n, target;
+    cout << "Enter size of the array: ";
+    cin >> n;
+    cout << "Enter elements:\n";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
     }
-    cout<<"Enter target: ";
-    cin>>target;
-    
-    On2(arr,n,target);
-    cout<<"\nThe indexes are: ";
-    On(arr,n,target);
+    cout << "Enter target: ";
+    cin >> target;
 
+    On2(arr, n, target);
+    cout << "\nThe indexes are: ";
+    On(arr, n, target);
 
     return 0;
 }
