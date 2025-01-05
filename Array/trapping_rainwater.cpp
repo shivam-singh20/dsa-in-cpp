5cc0b33813acbd8970d8f80f7e89c982178405cd
shivam-singh20 <shivamks5689@gmail.com>
2025-01-05 13:41:27 +0530

    Add code

--- Array/trapping_rainwater.cpp
+++ Array/trapping_rainwater.cpp
@@ -1,6 +1,42 @@
 #include <iostream>
 using namespace std;
 
+// For T.C - O(n) and S.C - O(1)
+int Maximum_Water(int heights[], int n)
+{
+    int maximum_height = heights[0], index = 0;
+
+    // Calculating Maximum height
+    for (int i = 1; i < n; i++)
+    {
+        if (maximum_height < heights[i])
+        {
+            maximum_height = heights[i];
+            index = i;
+        }
+    }
+    int left_max = 0, right_max = 0, sum = 0;
+
+    // Solving left part
+    for (int i = 0; i < index; i++)
+    {
+        if (left_max > heights[i])
+            sum += left_max - heights[i];
+        else
+            left_max = heights[i];
+    }
+
+    // Solving right part
+    for (int i = n - 1; i > index; i--)
+    {
+        if (right_max > heights[i])
+            sum += right_max - heights[i];
+        else
+            right_max = heights[i];
+    }
+    return sum;
+}
+
 // For T.C - O(n) and S.C - O(n)
 
 int Total_water(int heights[], int n)
@@ -24,7 +60,7 @@ int Total_water(int heights[], int n)
     // {
     //     cout << right_max[i];
     // }
-    int ans = 0,Mini_height=0;
+    int ans = 0, Mini_height = 0;
     for (int i = 0; i < n; i++)
     {
         Mini_height = min(left_max[i], right_max[i]);
@@ -44,7 +80,7 @@ int main()
     {
         cin >> heights[i];
     }
-    cout << "Maximum rain water that can be trapped is " << Total_water(heights, n) << " units.";
-
+    cout << "Maximum rain water that can be trapped is " << Total_water(heights, n) << " units.\n";
+    cout << "Maximum rain water that can be trapped is " << Maximum_Water(heights, n) << " units.";
     return 0;
 }
