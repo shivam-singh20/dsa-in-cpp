577ef3353c06f451f6018836e424e4d67b404fa8
shivam-singh20 <shivamks5689@gmail.com>
2025-01-06 22:38:43 +0530

    Add code

--- Array/four_sum.cpp
+++ Array/four_sum.cpp
@@ -1,15 +1,46 @@
 #include <iostream>
-#include<vector>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
+// For T.C - O(n3) and S.C - O(1)
+bool n3(vector<int> &arr, int target)
+{
+    int n = arr.size();
+    sort(arr.begin(), arr.end());
+    for (int i = 0; i < n - 3; i++)
+    {
+        for (int j = i + 1; j < n - 2; j++)
+        {
+            int num = target - arr[i] - arr[j];
+            int start = j + 1, end = n - 1;
+            while (start < end)
+            {
+                if (arr[start] + arr[end] == num)
+                    return 1;
+                else if (arr[start] + arr[end] < num)
+                    start++;
+                else
+                    end--;
+            }
+        }
+    }
+    return 0;
+}
+
 // For T.C - O(n4) and S.C - O(1)
-bool n4(vector<int>& arr,int target){
-    int n=arr.size();
-    for(int i=0;i<n-3;i++){
-        for(int j=i+1;j<n-2;j++){
-            for(int k=j+1;k<n-1;k++){
-                for(int l=k+1;l<n;l++){
-                    if(arr[i]+arr[j]+arr[k]+arr[l]==target)
+bool n4(vector<int> &arr, int target)
+{
+    int n = arr.size();
+    for (int i = 0; i < n - 3; i++)
+    {
+        for (int j = i + 1; j < n - 2; j++)
+        {
+            for (int k = j + 1; k < n - 1; k++)
+            {
+                for (int l = k + 1; l < n; l++)
+                {
+                    if (arr[i] + arr[j] + arr[k] + arr[l] == target)
                         return true;
                 }
             }
@@ -18,17 +49,24 @@ bool n4(vector<int>& arr,int target){
     return false;
 }
 
-int main (){
-    int n,target;
-    cout<<"Enter size of the array: ";
-    cin>>n;
-    vector<int>arr(n);
-    cout<<"Enter numbers: \n";
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
+int main()
+{
+    int n, target;
+    cout << "Enter size of the array: ";
+    cin >> n;
+    vector<int> arr(n);
+    cout << "Enter numbers: \n";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
     }
     cout << "Enter target: ";
     cin >> target;
-    cout<<n4(arr,target);
+
+    // For T.C - O(n4) and S.C - O(1)
+    cout << n4(arr, target) << endl;
+
+    // For T.C - O(n3) and S.C - O(1)
+    cout << n3(arr, target) << endl;
     return 0;
 }
