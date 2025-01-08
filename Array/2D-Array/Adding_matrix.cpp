775c4796c6b14651857e5e326678b085469d4341
shivam-singh20 <shivamks5689@gmail.com>
2025-01-08 21:57:39 +0530

    Add code

--- Array/2D-Array/Adding_matrix.cpp
+++ Array/2D-Array/Adding_matrix.cpp
@@ -5,10 +5,11 @@ int main (){
     int arr1[3][4]={1,2,3,4,5,6,7,8,9,10,11,12};
     int arr2[3][4]={13,14,15,16,17,18,19,20,21,22,23,24};
     int ans[3][4];
-    for(int i=0;i<3;i++){
-        for(int j=0;j<4;j++){
-            ans[i][j] = arr1[i][j] + arr2[i][j];
-            cout<<ans[i][j]<<" ";
+    //For T.C - O(row*col)
+    for(int row=0;row<3;row++){
+        for(int col=0;col<4;col++){
+            ans[row][col] = arr1[row][col] + arr2[row][col];
+            cout<<ans[row][col]<<" ";
         }
     }
     return 0;
