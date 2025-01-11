8c33cc5432b94467f28c6091420555330b1a9464
shivam-singh20 <shivamks5689@gmail.com>
2025-01-11 22:56:43 +0530

    Add code

--- Array/2D-Array/Snake_Pattern.cpp
+++ Array/2D-Array/Snake_Pattern.cpp
@@ -3,36 +3,43 @@
 #include <algorithm>
 using namespace std;
 
-vector<int> Snake_Pattern(vector<vector<int>> matrix){
+vector<int> Snake_Pattern(vector<vector<int>> matrix,int n){
     vector<int>ans;
-    int row = matrix.size(), col = matrix[0].size();
-    int start=0, end=row-1;
-
-    while(start<=end){
-        for(int i=0; i<=end; i++)
-            ans.push_back(matrix[start][i]);
-        start++;
-        for(int j=end; j>=0; j--)
-            ans.push_back(matrix[start][j]);
+    for(int i=0; i<n; i++){
+        if(i%2==0){
+            for(int j=0; j<n; j++)
+                ans.push_back(matrix[i][j]);
+        }
+        else{
+            for(int j=n-1; j>=0; j--)
+                ans.push_back(matrix[i][j]);
+        }
     }
+    return ans;
 }
-
+  
 int main (){
-    int row = 0, col = 0;
-    cout << "Enter rows: ";
-    cin >> row;
-    cout << "Enter columns: ";
-    cin >> col;
+    int n;
+    
+    cout<<"Enter size of the matrix: ";
+    cin>>n;
 
-    vector<vector<int>> matrix(row,vector<int>(col));
+    vector<vector<int>> matrix(n,vector<int>(n));
 
     cout << "Enter elements of rows and columns: \n";
-    for (int i = 0; i < row; i++)
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < col; j++)
+        for (int j = 0; j < n; j++)
         {
             cin >> matrix[i][j];
         }
+    }
+    vector<int> result=Snake_Pattern(matrix,n);
+
+    // Print the result
+    cout << "Snake Pattern Traversal: ";
+    for (int x : result) {
+        cout << x << " ";
     }
     return 0;
 }
