27b63837e09633cd518bcff6db899d2fd3193639
shivam-singh20 <shivamks5689@gmail.com>
2025-01-12 11:32:09 +0530

    Add code

--- Array/2D-Array/Rotate_array.cpp
+++ Array/2D-Array/Rotate_array.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-int main (){
-    return 0;
+//For T.C - O(n2) and S.C - O(n2)
+void Rotate_by_90(vector<vector<int>>matrix, int n) {
+    vector<vector<int>> ans(n,vector<int>(n));
+    for(int i=0;i<n;i++){
+        for(int j=0; j<n; j++){
+            ans[j][n-1-i] = matrix[i][j];
+        }
+    }
+    cout<<"Rotated matrix by 90 is:\n";
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cout<<ans[i][j]<<" ";
+        }
+        cout<<endl;
+    }
 }
+ 
+int main (){
+    int n;
+    cout<<"Enter size of square matrix: ";
+    cin>>n;
+    vector<vector<int>> matrix(n,vector<int>(n));
+    cout<<"Enter elements of rows and columns:\n";
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++)
+            cin>>matrix[i][j];
+    }
+    Rotate_by_90(matrix,n);
+    return 0;
+}  
