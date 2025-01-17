a1c84f833d62496e9ecdec95ac96f5bc267c9c46
shivam-singh20 <shivamks5689@gmail.com>
2025-01-17 23:29:02 +0530

    Add code

--- Array/Strings/Sentence_is_pangram.cpp
+++ Array/Strings/Sentence_is_pangram.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
+bool Pangram(string &sen){
+    int n = sen.size();
+    vector<bool>alpha(26,0);
+
+    for(int i=0; i<n; i++)
+        alpha[sen[i]-'a'] = 1;
+
+    for(int i=0; i<26; i++){
+        if(alpha[i]==0)
+            return false;
+    }
+    return true;
+}
+
 int main (){
-return 0;
+    string sentence = "thequickbrownfoxjumpsoverthelazydog";
+    cout<<Pangram(sentence);
+    // return 0;
 }
