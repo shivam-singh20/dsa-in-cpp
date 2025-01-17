26ef182c9046698393fb7af39e06d45226c2c985
shivam-singh20 <shivamks5689@gmail.com>
2025-01-17 23:42:22 +0530

    Add code

--- Array/Strings/Sorting_String.cpp
+++ Array/Strings/Sorting_String.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main (){
-return 0;
+    string sentence = "adcsxfesgvsaads";
+    vector<int>alpha(26,0);
+
+    for(int i=0; i<sentence.size(); i++)
+        alpha[sentence[i]-'a']++;
+
+    string ans;
+    for(int i=0; i<26; i++){
+        char c = 'a' + i;
+        while(alpha[i])
+        {
+            ans = ans + c;
+            alpha[i]--;
+        }
+    }
+    cout<<ans;
+    return 0;
 }
