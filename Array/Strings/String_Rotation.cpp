d06b2694c729aaff2a00350f9c176d8b40ae0e34
shivam-singh20 <shivamks5689@gmail.com>
2025-01-17 14:18:34 +0530

    Add code

--- Array/Strings/String_Rotation.cpp
+++ Array/Strings/String_Rotation.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
 using namespace std;
 
+bool isRotated(string str1, string str2)
+    {
+        // Your code here
+        int n = str1.length();
+        bool clockwise = true, anticlockwise = true;
+        for (int i = 0; i < n; i++)
+        {
+            if (str1[i] != str2[(i + 2) % n])
+            {
+                clockwise = false; // not rotated clockwise
+                break;
+            }
+        }
+ 
+        for (int i = 0; i < n; i++)
+        {
+            if (str1[(i + 2) % n] != str2[i])
+            {
+                anticlockwise = false; // not rotated anticlockwise
+                break;
+            }
+        }
+ 
+        return clockwise or anticlockwise; // if any of both is true, return true
+    }
+
 int main (){
-return 0;
+    string s1 = "SHIVAM", s2 = "sMSHIV";
+    cout<<isRotated(s1,s2);   
+    return 0;
 }
