b8c0444970e13f69561517a72c44418c5421477d
shivam-singh20 <shivamks5689@gmail.com>
2025-01-17 13:22:21 +0530

    Add code

--- Array/Strings/Defaning_IP_Address.cpp
+++ Array/Strings/Defaning_IP_Address.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 int main (){
-return 0;
+    string s="255.100.25.60";
+    int index = 0;
+    string ans;
+    cout<<s<<endl;
+
+    while(index<s.size()){
+        if(s[index]=='.')
+            ans+="[.]";
+        else
+            ans+=s[index];
+        index++;
+    }
+    
+    cout<<ans;
+    return 0;
 }
