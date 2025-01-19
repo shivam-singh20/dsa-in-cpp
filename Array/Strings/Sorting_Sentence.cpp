8c8193e32c28070afca2cd5c4d249bfa9f4264f8
shivam-singh20 <shivamks5689@gmail.com>
2025-01-19 23:35:08 +0530

    Add code

--- Array/Strings/Sorting_Sentence.cpp
+++ Array/Strings/Sorting_Sentence.cpp
@@ -1,6 +1,48 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
+string sortSentence(string s) {
+        vector<string>ans(10);
+        string temp;
+        int count = 0, index=0;
+        while(index<s.size())
+        {
+            if(s[index] == ' ')
+            {
+                int position = temp[temp.size()-1] - '0';
+                temp.pop_back();
+                ans[position] = temp;
+                temp.clear();
+                count++; 
+            }
+
+            else
+                temp+=s[index];
+
+            index++;
+        }
+        int position = temp[temp.size()-1] - '0';
+        temp.pop_back();
+        ans[position] = temp;
+        temp.clear();
+        count++;
+
+        //Printing the string
+        for(int i=1; i<=count; i++){
+            temp+=ans[i];
+            temp+=' ';
+        }
+        temp.pop_back();
+
+        return temp;
+    }
+
 int main (){
-return 0;
+    string s;
+
+    //Number ranges from 1-9
+    getline(cin,s); //Shivam4 name2 my is3
+    cout<<"Sorted sentence is: "<<sortSentence(s);
+    return 0;
 }
