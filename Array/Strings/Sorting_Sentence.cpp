cfc3576dc5fa7371a69fed647f93dcc86ebea62e
shivam-singh20 <shivamks5689@gmail.com>
2025-01-19 23:35:54 +0530

    Add code

--- Array/Strings/Sorting_Sentence.cpp
+++ Array/Strings/Sorting_Sentence.cpp
@@ -2,47 +2,50 @@
 #include <vector>
 using namespace std;
 
-string sortSentence(string s) {
-        vector<string>ans(10);
-        string temp;
-        int count = 0, index=0;
-        while(index<s.size())
+string sortSentence(string s)
+{
+    vector<string> ans(10);
+    string temp;
+    int count = 0, index = 0;
+    while (index < s.size())
+    {
+        if (s[index] == ' ')
         {
-            if(s[index] == ' ')
-            {
-                int position = temp[temp.size()-1] - '0';
-                temp.pop_back();
-                ans[position] = temp;
-                temp.clear();
-                count++; 
-            }
-
-            else
-                temp+=s[index];
-
-            index++;
+            int position = temp[temp.size() - 1] - '0';
+            temp.pop_back();
+            ans[position] = temp;
+            temp.clear();
+            count++;
         }
-        int position = temp[temp.size()-1] - '0';
-        temp.pop_back();
-        ans[position] = temp;
-        temp.clear();
-        count++;
 
-        //Printing the string
-        for(int i=1; i<=count; i++){
-            temp+=ans[i];
-            temp+=' ';
-        }
-        temp.pop_back();
+        else
+            temp += s[index];
 
-        return temp;
+        index++;
     }
+    int position = temp[temp.size() - 1] - '0';
+    temp.pop_back();
+    ans[position] = temp;
+    temp.clear();
+    count++;
 
-int main (){
+    // Printing the string
+    for (int i = 1; i <= count; i++)
+    {
+        temp += ans[i];
+        temp += ' ';
+    }
+    temp.pop_back();
+
+    return temp;
+}
+
+int main()
+{
     string s;
 
-    //Number ranges from 1-9
-    getline(cin,s); //Shivam4 name2 my is3
-    cout<<"Sorted sentence is: "<<sortSentence(s);
+    // Number ranges from 1-9
+    getline(cin, s); // Shivam4 name2 my1 is3
+    cout << "Sorted sentence is: " << sortSentence(s);
     return 0;
 }
