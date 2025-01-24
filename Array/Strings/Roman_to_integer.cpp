ec3d41df402775c593314ce8ea9612475282aa2c
shivam-singh20 <shivamks5689@gmail.com>
2025-01-24 23:22:44 +0530

    Add code

--- Array/Strings/Roman_to_integer.cpp
+++ Array/Strings/Roman_to_integer.cpp
@@ -3,30 +3,38 @@ using namespace std;
 
 int num(char c)
 {
-    if(c == 'I')
+    if (c == 'I')
         return 1;
-    else if(c == 'V')
+    else if (c == 'V')
         return 5;
-    else if(c == 'X')
+    else if (c == 'X')
         return 10;
-    else if(c == 'L')
+    else if (c == 'L')
         return 50;
-    else if(c == 'C')
+    else if (c == 'C')
         return 100;
-    else if(c == 'D')
+    else if (c == 'D')
         return 500;
     else // if c == M
         return 1000;
 }
 
-int main (){
+int main()
+{
     string s;
-    cout<<"Enter roman number: ";
-    getline(cin,s);
+    cout << "Enter roman number: ";
+    getline(cin, s);
     int sum = 0, index = 0;
-    while(index<s.size()-1)
+    while (index < s.size() - 1)
     {
-        if()
+        if ( num(s[index]) < num(s[index+1]) )
+            sum-=num(s[index]);
+        else
+            sum+=num(s[index]);
+        index++;
     }
-    return 0;    
+    sum+=num(s[s.size()-1]);
+    cout<<"Integer number is: "<<sum;
+
+    return 0;
 }
