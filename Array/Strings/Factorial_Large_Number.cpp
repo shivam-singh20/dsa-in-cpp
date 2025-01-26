d3eec15e5bd18629c622ceefde9e4c7a77eb83d5
shivam-singh20 <shivamks5689@gmail.com>
2025-01-26 21:51:16 +0530

    Add code

--- Array/Strings/Factorial_Large_Number.cpp
+++ Array/Strings/Factorial_Large_Number.cpp
@@ -1,11 +1,43 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-string factorial(string num){
-       
+vector<int> Fact(int num) {
+    vector<int> ans(1, 1);
+    while (num > 1) {
+        int carry = 0, result;
+        for (int i = 0; i < ans.size(); i++) {
+            result = ans[i] * num + carry;
+            carry = result / 10;
+            ans[i] = result % 10;
+        }
+        while (carry) {
+            ans.push_back(carry % 10);
+            carry = carry / 10;
+        }
+        num--;
+    }
+    reverse(ans.begin(), ans.end());
+    return ans;
 }
 
-int main (){
-int fact;
-     return 0;
-}   
+int main() {
+    int num;
+    cout << "Enter number: ";
+    cin >> num;
+
+    if (num < 0) {
+        cout << "Factorial is not defined for negative numbers." << endl;
+        return 0;
+    }
+
+    vector<int> result = Fact(num);
+    cout << "Factorial of " << num << " is: ";
+    for (int digit : result) {
+        cout << digit;
+    }
+    cout << endl;
+
+    return 0;
+}
