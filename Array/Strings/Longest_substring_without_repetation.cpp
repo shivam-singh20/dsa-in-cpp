88a05e8efcbf16266c61e926bd3f9d015e8a95a5
shivam-singh20 <shivamks5689@gmail.com>
2025-01-27 23:22:57 +0530

    Add code

--- Array/Strings/Longest_substring_without_repetation.cpp
+++ Array/Strings/Longest_substring_without_repetation.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 // Sliding Window Protocol
 int main (){
+    string s; //abcdecbeadf
+    cout<<"Enter string: ";
+    getline(cin,s);
+
+    vector<bool>count(256,0);
+    int first = 0, second = 0, length =0;
+    while(second<s.size())
+    {
+        while(count[s[second]])
+        {
+            count[s[first]] = 0;
+            first++;
+        }
+        count[s[second]] = 1;
+        length = max(length,second-first+1);
+        second++;
+    }
+    cout<<"Length of Longest Substring without any repetation is: "<<length;
     return 0;
 }
