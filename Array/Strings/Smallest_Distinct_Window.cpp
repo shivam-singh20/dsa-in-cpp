304dde3252ec7208739fa22aa5207f0a21a7e276
shivam-singh20 <shivamks5689@gmail.com>
2025-01-28 21:14:50 +0530

    Add code

--- Array/Strings/Smallest_Distinct_Window.cpp
+++ Array/Strings/Smallest_Distinct_Window.cpp
@@ -1,7 +1,52 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main (){
-    
+    string s;
+    // cout<<"Enter string: ";
+    getline(cin,s);
+
+    vector<int>count(256,0);
+    int first = 0, second = 0, length = s.size(), different = 0;
+
+    //Finding different characters in string
+    while(first<s.size())
+    {
+        if(count[s[first]] == 0)
+            different++;
+        
+        count[s[first]]++;
+        first++;
+    }
+
+    for(int i=0; i<256; i++)
+        count[i] = 0;
+    first = 0;
+
+    while(second<s.size())
+    {
+        while(different && second<s.size())
+        {
+            if(count[s[second]] == 0)
+                different--;
+
+            count[s[second]]++;
+            second++;
+        }
+        length = min(length,second-first);
+    }
+
+    while(different != 1)
+    {
+        length = min(length,second-first);
+        count[s[first]]--;
+
+        if(count[s[first]] == 0)
+            different++;
+        
+        first++;
+    }
+    cout<<"Size of smallest distinct window is: "<<length;
     return 0;
 }
