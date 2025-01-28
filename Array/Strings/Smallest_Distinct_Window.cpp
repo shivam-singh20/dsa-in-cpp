5b8fcb4267f03450d164da3a582e2ed05f30fd73
shivam-singh20 <shivamks5689@gmail.com>
2025-01-28 21:36:14 +0530

    Add code

--- Array/Strings/Smallest_Distinct_Window.cpp
+++ Array/Strings/Smallest_Distinct_Window.cpp
@@ -4,49 +4,50 @@ using namespace std;
 
 int main (){
     string s;
-    // cout<<"Enter string: ";
+    cout<<"Enter string: ";
     getline(cin,s);
 
     vector<int>count(256,0);
-    int first = 0, second = 0, length = s.size(), different = 0;
-
-    //Finding different characters in string
-    while(first<s.size())
-    {
-        if(count[s[first]] == 0)
-            different++;
+        int first = 0, second = 0, length = s.size(), diff = 0;
         
-        count[s[first]]++;
-        first++;
-    }
-
-    for(int i=0; i<256; i++)
-        count[i] = 0;
-    first = 0;
-
-    while(second<s.size())
-    {
-        while(different && second<s.size())
+        //Finding different characters in given string
+        while(first<s.size())
         {
-            if(count[s[second]] == 0)
-                different--;
-
-            count[s[second]]++;
-            second++;
+            if(count[s[first]] == 0)
+                diff++;
+            
+            count[s[first]]++;
+            first++;
         }
-        length = min(length,second-first);
-    }
-
-    while(different != 1)
-    {
-        length = min(length,second-first);
-        count[s[first]]--;
-
-        if(count[s[first]] == 0)
-            different++;
         
-        first++;
-    }
+        for(int i=0; i<256; i++)
+            count[i] = 0;
+        first = 0;
+        
+        while(second<s.size())
+        {
+            //Jabtak different character exist kar rha ho
+            while(diff && second<s.size())
+            {
+                if(count[s[second]] == 0)
+                    diff--;
+                    
+                count[s[second]]++;
+                second++;
+            }
+            length = min(length,second-first);
+            
+            while(diff != 1)
+            {
+                length = min(length,second-first);
+                count[s[first]]--;
+            
+                if(count[s[first]] == 0)
+                    diff++;
+                
+                first++;
+            }
+        }
     cout<<"Size of smallest distinct window is: "<<length;
     return 0;
 }
