01ec8a5f0f291e67b302a3d1dea68d94204c8fa5
shivam-singh20 <shivamks5689@gmail.com>
2025-02-09 21:18:08 +0530

    Add code

--- Array/Strings/Longest_Prefix_Suffix.cpp
+++ Array/Strings/Longest_Prefix_Suffix.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 //KMP Algorithm
@@ -7,6 +8,29 @@ int main (){
     string s;
     cout<<"Enter string: ";
     cin>>s;
-     
+    vector<int> lps(s.size(),0);
+    int prefix = 0, suffix = 1;
+
+    while(suffix<s.size())
+    {
+        if(s[prefix]==s[suffix])
+        {
+            lps[suffix] = prefix+1;
+            prefix++,suffix++;
+        }
+        else
+        {
+            if(prefix==0)
+            {
+                lps[suffix] = 0;
+                suffix++;
+            }
+            else
+                prefix = lps[prefix-1];
+        }
+    }
+
+    cout<<"Longest Prefix Suffix is: "<<lps[s.size()-1];
+
     return 0;
 }
