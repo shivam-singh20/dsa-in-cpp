354541dfe2e89ed9b49de645c91abcb4e6ea030f
shivam-singh20 <shivamks5689@gmail.com>
2025-05-31 15:32:32 +0530

    Add code

--- Array/Sorting/selection_sort.cpp
+++ Array/Sorting/selection_sort.cpp
@@ -1,27 +1,51 @@
 #include <iostream>
 using namespace std;
 
+void selection(int arr[], int n)
+{
+    //Applying selection sort for ascending order
+        for(int i=0; i<n; i++)
+        {
+            //Same as concept code of selection sort
+            int index = i;
+            for(int j = i+1; j<n; j++)
+            {
+                if(arr[j] < arr[index])
+                {
+                    index = j;
+                }
+            }
+            swap(arr[i],arr[index]);
+        }
+        for(int i=0; i<n; i++)
+        {
+            cout<<arr[i]<<" ";
+        }
+}
+
 int main()
 {
-    int arr[1000], n;
-    cout << "Enter size of the array: ";
-    cin >> n;
-    cout << "Enter numbers in the array\n";
-    for (int i = 0; i < n; i++)
-        cin >> arr[i];
+    int arr[1000],n;
+    cout<<"Enter size of the array: ";
+    cin>> n;
+    cout<<"Enter elements of array:\n";
 
-    for (int i = 0; i <= n - 1; i++)
+    for(int i=0; i<n; i++)
     {
-        int index = i;
-        for (int j = i + 1; j < n; j++) 
-        {
-            if (arr[j] < arr[index])
-            {
-                swap(arr[j], arr[index]);
-            }
-        }
-        cout << arr[i] << " ";
+        cin>>arr[i];
     }
-    cin >> n;
+
+    // Concept in selection sort
+    // int index=0;
+    // for(int i=1; i<n; i++)
+    // {
+    //     if(arr[i]<arr[index])
+    //     {
+    //         index = i;
+    //     }
+    // }
+    // cout<<"Smallest index at which the value is present is: "<<index;
+
+    selection(arr,n);
     return 0;
 }
