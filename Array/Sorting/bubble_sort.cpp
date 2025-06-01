0cae19f910f48328b80ecc46a0f294485490e533
shivam-singh20 <shivamks5689@gmail.com>
2025-06-01 19:12:19 +0530

    Add code

--- Array/Sorting/bubble_sort.cpp
+++ Array/Sorting/bubble_sort.cpp
@@ -1,34 +1,79 @@
 #include <iostream>
 using namespace std;
 
+//Applying ascending order in bubble sort
+void bubble_asc(int arr[], int n)
+{
+    for(int i=n-2; i>=0; i--)
+    {
+        bool if_sorted = 0;
+        for(int j=0; j<=i; j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                if_sorted = 1;
+                swap(arr[j],arr[j+1]);
+            }
+        }
+        if(if_sorted == 0)
+            break;
+    }
+
+    cout << "Sorted in ascending order: ";
+    for(int i=0; i<n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+}
+
+//Applying descending order in bubble sort
+void bubble_desc(int arr[], int n)
+{
+    for(int i=n-2; i>=0; i--)
+    {
+        bool if_sorted = 0;
+        for(int j=0; j<=i; j++)
+        {
+            if(arr[j]<arr[j+1])
+            {
+                if_sorted = 1;
+                swap(arr[j],arr[j+1]);
+            }
+        }
+        if(if_sorted == 0)
+            break;
+    }
+
+    cout << "Sorted in descending order: ";
+    for(int i=0; i<n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+}
+
 int main()
 {
     int arr[1000], n;
     cout << "Enter size of the array: ";
     cin >> n;
-    cout << "Enter numbers in the array:\n";
-    for (int i=0; i < n; i++){
+    cout << "Enter elements of array:\n";
 
+    for (int i = 0; i < n; i++)
+    {
         cin >> arr[i];
     }
 
-    for (int i = n - 2; i > 0; i--)
-    {
-        bool swapped = 0;
-        for (int j = 0; j <= i; j++)
-        {
-            if (arr[j] < arr[j + 1])
-            {
-                swapped = 1;
-                swap(arr[j], arr[j + 1]);
-            }
-        }
-        if (swapped == 0)
-            break;
-    }
-    for (int i = 0; i < n; i++)
-    {
-        cout << arr[i] << " ";
-    }
+    //Concept in bubble sorting
+    // for(int i=0; i<n-1; i++)
+    // {
+    //     if(arr[i]>arr[i+1])
+    //     {
+    //         swap(arr[i],arr[i+1]);
+    //     }
+    // }
+
+    bubble_asc(arr,n);
+    cout<<endl;
+    bubble_desc(arr,n);
     return 0;
 }
