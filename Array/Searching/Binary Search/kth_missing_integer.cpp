184c4c951eb8ac0ee76d1fbbc9efd719e9f803b4
shivam-singh20 <shivamks5689@gmail.com>
2025-06-20 12:26:17 +0530

    Add code

--- Array/Searching/Binary Search/kth_missing_integer.cpp
+++ Array/Searching/Binary Search/kth_missing_integer.cpp
@@ -1,7 +1,33 @@
 #include<iostream>
 using namespace std;
 
-int missing_integer(int arr[],int n,int k){
+int missing_integer1(int arr[],int n, int k)
+{
+    //T.C - O(n)
+
+    int missing = 0, current_pos = 1, i = 0;
+    while(true)
+    {
+        if(arr[i]==current_pos)
+        {
+            i++;
+        }
+        else{
+            missing++;
+            if(missing==k)
+            {
+                return current_pos ;
+            }
+        }
+        current_pos++;
+    }
+    return -1;
+}
+
+int missing_integer2(int arr[],int n,int k){
+
+    //T.C - O(log(n))
+
     int start=0,end=n-1,mid,ans=n;
     while(start<=end){
         mid=start+(end-start)/2;
@@ -23,10 +49,12 @@ int main (){
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-    cout<<"Enter which kth number is to find: ";
+    cout<<"Enter value of k: ";
     cin>>k;
 
-    cout<<"Kth number is "<<missing_integer(arr,n,k);
+    cout<<"Kth missing number is "<<missing_integer1(arr,n,k)<<endl;
+
+    cout<<"Kth missing number is "<<missing_integer2(arr,n,k);
 
     return 0;
 }
