02c9ca939e27be6825709f32b276ed2c9112b9cf
shivam-singh20 <shivamks5689@gmail.com>
2025-07-02 20:05:16 +0530

    Add code

--- Array/Prefix_Suffix and Subarray/largest_sum_subarray.cpp
+++ Array/Prefix_Suffix and Subarray/largest_sum_subarray.cpp
@@ -3,48 +3,49 @@
 #include <climits>
 using namespace std;
 
-int On2(vector<int>arr, int n)
+int On2(vector<int> arr, int n)
 {
     int maxi = INT_MIN;
-    for(int i=0; i<n; i++)
+    for (int i = 0; i < n; i++)
     {
         int prefix_sum = 0;
-        for(int j=i; j<n; j++)
+        for (int j = i; j < n; j++)
         {
             prefix_sum += arr[j];
-            maxi = max(maxi,prefix_sum);
+            maxi = max(maxi, prefix_sum);
         }
     }
     return maxi;
 }
 
-//Kadanes algorithm 
-int On(vector<int>arr, int n)
+// Kadanes algorithm
+int On(vector<int> arr, int n)
 {
-    int maxi = INT_MIN, prefix_sum=0;
-    for(int i=0; i<n; i++)
+    int maxi = INT_MIN, prefix_sum = 0;
+    for (int i = 0; i < n; i++)
     {
         prefix_sum += arr[i];
-        if(prefix_sum<0)
+        if (prefix_sum < 0)
             prefix_sum = 0;
-        maxi = max(maxi,prefix_sum);
+        maxi = max(maxi, prefix_sum);
     }
     return maxi;
 }
 
-int main (){
+int main()
+{
     int n;
-    cout<<"Enter size of the array: ";
-    cin>>n;
-    
-    vector<int>arr(n);
-    for(int i=0; i<n; i++)
+    cout << "Enter size of the array: ";
+    cin >> n;
+
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
     {
-        cin>>arr[i];
+        cin >> arr[i];
     }
 
-    cout<<"Largest sum in contiguous subarray is: "<<On2(arr,n);
-    cout<<"\nLargest sum in contiguous subarray is: "<<On(arr,n);
+    cout << "Largest sum in contiguous subarray is: " << On2(arr, n);
+    cout << "\nLargest sum in contiguous subarray is: " << On(arr, n);
 
     return 0;
 }
