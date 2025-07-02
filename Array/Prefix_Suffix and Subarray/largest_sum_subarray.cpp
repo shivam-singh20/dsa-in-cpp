290e7a08d70d5a5ed108e3384a6744666c22e536
shivam-singh20 <shivamks5689@gmail.com>
2025-07-02 20:04:39 +0530

    Add code

---
+++ Array/Prefix_Suffix and Subarray/largest_sum_subarray.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+
+int On2(vector<int>arr, int n)
+{
+    int maxi = INT_MIN;
+    for(int i=0; i<n; i++)
+    {
+        int prefix_sum = 0;
+        for(int j=i; j<n; j++)
+        {
+            prefix_sum += arr[j];
+            maxi = max(maxi,prefix_sum);
+        }
+    }
+    return maxi;
+}
+
+//Kadanes algorithm 
+int On(vector<int>arr, int n)
+{
+    int maxi = INT_MIN, prefix_sum=0;
+    for(int i=0; i<n; i++)
+    {
+        prefix_sum += arr[i];
+        if(prefix_sum<0)
+            prefix_sum = 0;
+        maxi = max(maxi,prefix_sum);
+    }
+    return maxi;
+}
+
+int main (){
+    int n;
+    cout<<"Enter size of the array: ";
+    cin>>n;
+    
+    vector<int>arr(n);
+    for(int i=0; i<n; i++)
+    {
+        cin>>arr[i];
+    }
+
+    cout<<"Largest sum in contiguous subarray is: "<<On2(arr,n);
+    cout<<"\nLargest sum in contiguous subarray is: "<<On(arr,n);
+
+    return 0;
+}
