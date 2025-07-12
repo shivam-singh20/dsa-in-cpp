6b2c1c08bf59ef5886b68e0cb6340e1797297523
shivam-singh20 <shivamks5689@gmail.com>
2025-07-12 13:21:35 +0530

    Add code

--- Functions/practice.cpp
+++ Functions/practice.cpp
@@ -1,36 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main()
-{
-    int row=0,col=0;
-    cout<<"Enter size of row: ";
-    cin>>row;
-    cout<<"Enter size of column: ";
-    cin>>col;
 
-    cout<<"Enter your matrix:\n";
-    int arr[10][10];
-    for(int i=0; i<row; i++)
-        for(int j=0; j<col; j++)
-            cin>>arr[i][j];
-    
-    for(int i=0; i<row; i++)
-    {
-        for(int j=0; j<col; j++)
-        {
-            cout<<arr[i][j]<<" ";
-        }
-            cout<<endl;
-    }
-    
-    for(int i=0; i<row; i++)
-    {
-        int col_sum = 0;
-        for(int j=0; j<col; j++)
-        {
-            col_sum += arr[j][i];
-        }
-        cout<<col_sum<<" ";
-    }
+int main (){
+    string s;
+    getline(cin,s);
+    cout<<s;
+    cout<<endl<<s.size();
+    string s1 = "shivam is a \"good\" boy";
+    cout<<s1;
     return 0;
 }
