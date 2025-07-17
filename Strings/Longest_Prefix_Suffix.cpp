47c485c57f26d831c5484990543f1340e703827c
shivam-singh20 <shivamks5689@gmail.com>
2025-07-17 22:55:12 +0530

    Add code

--- Strings/Longest_Prefix_Suffix.cpp
+++ Strings/Longest_Prefix_Suffix.cpp
@@ -4,6 +4,40 @@ using namespace std;
 
 //KMP Algorithm
 
+// T.C- O(n2)
+int lps1(string s)
+{
+   int n = s.size();
+    vector<string>prefix(s.size()-1);
+    vector<string>suffix(s.size()-1);
+    
+    for(int i=1; i<n; i++)
+    {
+        string temp = "";
+        for(int j=0; j<i; j++)
+        {
+            temp += s[j];
+        }
+        prefix[i-1] = temp;
+    }
+
+    for(int i=1; i<n; i++)
+    {
+        string temp = "";
+        for(int j=n-i; j<n; j++)
+        {
+            temp += s[j];
+        }
+        suffix[i-1] = temp;
+    }
+
+    for (int i = n - 2; i >= 0; i--) {
+        if (prefix[i] == suffix[i])
+            return prefix[i].length();
+    }
+    return 0;
+}
+
 int main (){
     string s;
     cout<<"Enter string: ";
@@ -32,6 +66,7 @@ int main (){
         }
     }
     cout<<"Length of longest prefix suffix is: "<<lps[s.size()-1];
+    cout<<"\nLength of longest prefix suffix is: "<<lps1(s);
 
     return 0;
 }
