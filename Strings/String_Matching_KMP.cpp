42deb2cbe4bc1c23a2084e6c640cb3232d86c317
shivam-singh20 <shivamks5689@gmail.com>
2025-07-18 00:50:20 +0530

    Add code

--- Strings/String_Matching_KMP.cpp
+++ Strings/String_Matching_KMP.cpp
@@ -3,29 +3,31 @@ using namespace std;
 
 // T.C - O(M*N) and S.C - O(1)
 
-int brute_force (string haystack, string needle) {
-        int n = haystack.size(), m = needle.size();
-        for(int i=0; i<=n-m; i++)
+int brute_force(string haystack, string needle)
+{
+    int n = haystack.size(), m = needle.size();
+    for (int i = 0; i <= n - m; i++)
+    {
+        int first = i, second = 0;
+        while (second < m)
         {
-            int first = i, second = 0;
-            while(second<m)
-            {
-                if(haystack[first] == needle[second])
-                    first++, second++;
-                else
-                    break;
-            }
-            if(second == m)
-                return first - second;
+            if (haystack[first] == needle[second])
+                first++, second++;
+            else
+                break;
         }
-        return -1;
+        if (second == m)
+            return first - second;
+    }
+    return -1;
 }
 
-int main (){
+int main()
+{
     string haystack = "Shivam";
     string needle = "hiv";
 
-    cout<<"String is matching from "<< brute_force(haystack, needle)<< " index.";
-    
+    cout << "String is matching from " << brute_force(haystack, needle) << " index.";
+
     return 0;
 }
