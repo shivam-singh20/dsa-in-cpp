8b72c35f4403e4b7ee2ede02d681821dff21f430
shivam-singh20 <shivamks5689@gmail.com>
2025-07-24 01:00:29 +0530

    Add code

--- Pointer/04_Function.cpp
+++ Pointer/04_Function.cpp
@@ -6,10 +6,23 @@ void increment(int *n)
     *n = *n + 1;
 }
 
+void first(char *ptr1,char *ptr2)
+{
+    while((*ptr1 = *ptr2))
+    {
+        ptr1++, ptr2++;
+    }
+}
+
 int main (){
-    int num = 10;
-    int *ptr = &num;
-    increment(&num);
-    cout<<num;
-    return 0;
+    // int num = 10;
+    // int *ptr = &num;
+    // increment(&num);
+    // cout<<num;
+    // return 0;
+
+    char name[] = "Shivam";
+    char sname[] = "Singh";
+    first(name,sname);
+    cout<<name;
 }
---
+++ Pointer/tempCodeRunnerFile.cpp
@@ -0,0 +1,5 @@
+int num = 10;
+    // int *ptr = &num;
+    // increment(&num);
+    // cout<<num;
+    // return 0;
