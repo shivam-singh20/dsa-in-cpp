9dae788b33655e5ea16bcd04ed936c37d0ddb674
shivam-singh20 <shivamks5689@gmail.com>
2025-07-25 15:39:21 +0530

    Add code

---
+++ Pointer/08_ndArray.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main (){
+
+    // Creating 2D Array
+    int n;
+    cout<<"Enter rows: ";
+    cin>>n;
+
+    int m;
+    cout<<"Enter col: ";
+    cin>>m;
+    
+    // Step 1: Creating an array that contains address of another array
+    int **ptr = new int *[n];
+
+    for(int i=0; i<n; i++)
+        ptr[i] = new int [m];
+
+    for(int i=0; i<n; i++)
+        for(int j=0; j<m; j++)
+            cin>>ptr[i][j];
+
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<m; j++)
+            cout<<ptr[i][j]<<" ";
+        cout<<endl;
+    }   
+
+    // Creating 3D Array
+    
+    return 0;
+}
