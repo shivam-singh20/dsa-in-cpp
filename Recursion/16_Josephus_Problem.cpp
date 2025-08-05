a67dce0cef15b9e7f7c7c5b715373b591006485f
shivam-singh20 <shivamks5689@gmail.com>
2025-08-05 14:33:48 +0530

    Add code

---
+++ Recursion/16_Josephus_Problem.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// T.C - O(n2) S.C - O(n)
+
+int Winner(vector<bool>person, int n, int k, int index, int person_left)
+{
+    // Base case
+    if(person_left == 1)
+    {
+        for(int i=0; i<n; i++)
+            if(person[i] == 0)
+                return i;
+    }
+
+    // Killing the kth person
+    int count = 0;
+    while(count<k)
+    {
+        if(person[index] == 0)
+            count++;
+        if(count == k)
+            break;
+        index = (index+1) % n;
+    }
+
+    person[index] = 1;
+
+    // Circular rotation
+    while(person[index] == 1)
+        index = (index + 1) % n;
+
+    return Winner(person,n,k,index,person_left-1);
+}
+
+// T.C - O(n) S.C - O(n)
+
+
+
+int main (){
+    int n;
+    cout<<"Enter number of players: ";
+    cin>>n;
+    int k;
+    cout<<"Enter orders to eliminate players: ";
+    cin>>k;
+
+    vector<bool>count_kill(n,0);
+
+    cout<<"Winner is player number: "<<Winner(count_kill,n,k,0,n) + 1;
+
+    return 0;
+}
