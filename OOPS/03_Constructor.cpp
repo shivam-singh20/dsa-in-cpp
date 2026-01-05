9b1636dd84f2dfbda02dcaf22d402b8bc131f891
shivam-singh20 <shivamks5689@gmail.com>
2026-01-05 16:21:40 +0530

    Add code

--- OOPS/03_Constructor.cpp
+++ OOPS/03_Constructor.cpp
@@ -11,14 +11,15 @@ class customer
 
     public:
 
-    //Default constructor
-    customer() //Name of the constructor is same as class name
+    //Default constructor - It is automatically created by the compiler when an object is created. We can manually create it also like created below after that compiler will not create the constructor for other object of same class we have to manually create it according to our needs.
+    customer() //Name of the constructor is same as class name.
     {
         name = "Shivam";
         account_number = 101;
         balance = 2000;
     }
 
+    
     //Parameterized constructor
     // customer(string a, int b, int c) //Parameters are present
     // {
@@ -34,12 +35,20 @@ class customer
         this -> account_number = account_number; //this pointer will point the class parameters present in customer class
     }
 
+
     //inline constructor
     // inline customer(string a, int b, int c) : name(a), account_number(b), balance(c){
 
     // }
 
-    //Copy constructor
+
+    //Copy constructor - It is also created by compiler by default
+    customer(customer &b) //We have to pass by reference
+    {
+        name = b.name;
+        account_number = b.account_number;
+        balance = b.balance;
+    }
 
     void display()
     {
@@ -51,16 +60,25 @@ class customer
 int main()
 {
     customer A1; //Constructor is invoked automatically at the time of object creation.
-    A1.display();
+    A1.display(); //Default constructor
 
     customer A2("Shivam",102,1500);
-    A2.display();
+    A2.display(); //Parameterized constructor
 
     customer A3("Shivam",103,4000);
-    A3.display();
+    A3.display(); //Parameterized constructor
 
     customer A4("Shivam",104,5000);
-    A4.display();
-    
+    A4.display(); //Inline constructor
+
+    customer A5(A3); //By default you can perform this also for copy constructor
+    A5.display(); // Copy constructor
+
+    customer A6;
+    A6 = A2;
+    A6.display();
+
+    customer B1(A4);
+    B1.display();
     
 }
--- OOPS/tempCodeRunnerFile.cpp
+++
@@ -1,6 +0,0 @@
-customer(string a, int b, int c) //Parameters are present
-    // {
-    //     name = a;
-    //     account_number = b;
-    //     balance = c;
-    // }
