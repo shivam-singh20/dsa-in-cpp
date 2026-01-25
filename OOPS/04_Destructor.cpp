3a0825c50a7188156c6570e31d789dbd50b30f5b
shivam-singh20 <shivamks5689@gmail.com>
2026-01-25 18:29:56 +0530

    Destructor is created

--- OOPS/04_Destructor.cpp
+++ OOPS/04_Destructor.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
 using namespace std;
 
+class Customer
+{
+    string name;
+    int account_number;
+    int balance;
+
+    public:
+
+    Customer(){ //Constructor
+        cout<<"I am a constructor"<<endl;
+    }
+
+    ~Customer(){ //Destructor
+        cout<<"I am a destructor"<<endl;
+    }
+};
+
 int main()
 {
-    cout<<"Heloo";
+    Customer c1;
+
 }
