b3d4dbd6607e5255446a2da85afec63b3ae165c3
shivam-singh20 <shivamks5689@gmail.com>
2026-02-10 19:53:41 +0530

    Add code

--- OOPS/05_Encapsulation_Abstraction.cpp
+++ OOPS/05_Encapsulation_Abstraction.cpp
@@ -38,8 +38,35 @@ class customer {
 
 };
 
+// access modifiers - Private , Protectd, Public
+
+class access_modifiers{
+    private:
+        int a;
+    protected:
+        int b;
+    public:
+        int c;
+
+    void access()
+    {
+        a = 10; // can access private within the class
+        b = 20; // can access protected within the class
+        c = 30; // can access public within the class
+    }
+};
+
+// External code
+
 int main()
 {
+    access_modifiers a1;
+    // a1.a; X cannot access private class in an external code
+    // a1.b; X cannot access protected class in an external code
+    a1.c; // can access public class in an external code
+
+
+
     customer c1("Shivam", 101, 1000);
     c1.display();
 
