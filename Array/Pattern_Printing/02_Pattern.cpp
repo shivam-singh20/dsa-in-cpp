b1b78f0f12ba7f0098cb484588a9a7e82b3cfa2c
shivam-singh20 <shivamks5689@gmail.com>
2026-02-23 14:57:48 +0530

    All complex and basic pattern learning done.

--- Array/Pattern_Printing/02_Pattern.cpp
+++ Array/Pattern_Printing/02_Pattern.cpp
@@ -128,4 +128,152 @@ int main()
         cout<<endl;
     }
 
+    cout<<"\nComplex Pattern - 5(Hollow Square)\n"<<endl;
+
+    // *****
+    // *   *
+    // *   *
+    // *   *
+    // *****
+
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n; j++){
+            if(i==0 || i==n-1 || j==0 || j==n-1)
+                cout<<"*";
+            else
+                cout<<" ";
+        }
+        cout<<endl;
+    }
+
+    cout<<"\nComplex Pattern - 5.1(Hollow Rectangle)\n"<<endl;
+
+    // **********
+    // *        *
+    // *        *
+    // *        *
+    // **********
+
+    int m = 10;
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<m; j++){
+            if(i==0 || i==n-1 || j==0 || j==m-1)
+                cout<<"*";
+            else
+                cout<<" ";
+        }
+        cout<<endl;
+    }
+
+    cout<<"\nComplex Pattern - 6(Hollow Triangle)\n"<<endl;
+
+    // *
+    // **
+    // * *
+    // *  *
+    // *****
+
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<=i; j++)
+        {
+            if(i==0 || i==n-1 || i==j || j==0)
+                cout<<"*";
+            else
+                cout<<" ";
+        }
+        cout<<endl;
+    }
+
+    cout<<"\nComplex Pattern - 7(Triangle)\n"<<endl;
+
+    //     *
+    //    * *
+    //   * * *
+    //  * * * *
+    // * * * * *
+
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n-i-1; j++)
+            cout<<" ";
+        for(int k=0; k<=i; k++)
+            cout<<"* ";
+        cout<<endl;
+    }
+
+    cout<<"\nComplex Pattern - 8(Reversed Triangle)\n"<<endl;
+
+    // * * * * *
+    //  * * * *
+    //   * * * 
+    //    * *
+    //     *
+
+    for(int i=n-1; i>=0; i--)
+    {
+        for(int j=0; j<n-i-1; j++)
+            cout<<" ";
+        for(int k=0; k<=i; k++)
+            cout<<"* ";
+        cout<<endl;
+    }
+
+    cout<<"\nComplex Pattern - 9(Butterfly)\n"<<endl;
+
+    // *                  *
+    // **                **
+    // ***              ***
+    // ****            ****
+    // *****          *****
+    // ******        ******
+    // *******      *******
+    // ********    ********
+    // *********  *********
+    // ********************
+    // *********  *********
+    // ********    ********
+    // *******      *******
+    // ******        ******
+    // *****          *****
+    // ****            ****
+    // ***              ***
+    // **                ** 
+    // *                  *
+    for(int i=1; i<=n; i++)
+    {
+        //Printing Stars
+        for(int j=1; j<=i; j++)
+            cout<<"*";
+
+        //Printing Spaces
+        for(int k=1; k<=(2*n)-(i*2); k++)
+            cout<<" ";
+
+        //Printing Stars
+        for(int j=i; j>=1; j--)
+            cout<<"*";
+
+    cout<<endl;
+    }
+
+
+    for(int i=n-1; i>=1; i--)
+    {
+        //Printing Stars
+        for(int j=1; j<=i; j++)
+            cout<<"*";
+
+        //Printing Spaces
+        for(int k=1; k<=(2*n)-(i*2); k++)
+            cout<<" ";
+
+        //Printing Stars
+        for(int j=i; j>=1; j--)
+            cout<<"*";
+
+    cout<<endl;
+    }
 }
--- Array/Pattern_Printing/03_Pattern.cpp
+++ Array/Pattern_Printing/03_Pattern.cpp
@@ -1,105 +1,210 @@
 #include<iostream>
 using namespace std;
 
+// Numbers Pattern
 int main()
 {
     int n;
     cout << "Size till pattern should print: ";
     cin >> n;
 
-    // cout<<"\nComplex Pattern - 5(Hollow Square)\n"<<endl;
+    cout<<"\n1st Pattern - Square Number Pattern\n";
+    cout<<endl;
 
-    // for(int i=0; i<n; i++)
-    // {
-    //     for(int j=0; j<n; j++){
-    //         if(i==0 || i==n-1 || j==0 || j==n-1)
-    //             cout<<"*";
-    //         else
-    //             cout<<" ";
-    //     }
-    //     cout<<endl;
-    // }
-
-    // cout<<"\nComplex Pattern - 5.1(Hollow Rectangle)\n"<<endl;
-
-    // int m = 10;
-    // for(int i=0; i<n; i++)
-    // {
-    //     for(int j=0; j<m; j++){
-    //         if(i==0 || i==n-1 || j==0 || j==m-1)
-    //             cout<<"*";
-    //         else
-    //             cout<<" ";
-    //     }
-    //     cout<<endl;
-    // }
-
-    // cout<<"\nComplex Pattern - 6(Hollow Triangle)\n"<<endl;
-
-    // for(int i=0; i<n; i++)
-    // {
-    //     for(int j=0; j<=i; j++)
-    //     {
-    //         if(i==0 || i==n-1 || i==j || j==0)
-    //             cout<<"*";
-    //         else
-    //             cout<<" ";
-    //     }
-    //     cout<<endl;
-    // }
-
-    // cout<<"\nComplex Pattern - 7\n"<<endl;
-
-    // for(int i=0; i<n; i++)
-    // {
-    //     for(int j=0; j<n-i-1; j++)
-    //         cout<<" ";
-    //     for(int k=0; k<=i; k++)
-    //         cout<<"* ";
-    //     cout<<endl;
-    // }
-
-    // cout<<"\nComplex Pattern - 8\n"<<endl;
-
-    // for(int i=n-1; i>=0; i--)
-    // {
-    //     for(int j=0; j<n-i-1; j++)
-    //         cout<<" ";
-    //     for(int k=0; k<=i; k++)
-    //         cout<<"* ";
-    //     cout<<endl;
-    // }
-
-    cout<<"\nComplex Pattern - 9\n"<<endl;
+// 1 2 3 4 5
+// 1 2 3 4 5
+// 1 2 3 4 5
+// 1 2 3 4 5
+// 1 2 3 4 5
 
     for(int i=0; i<n; i++)
     {
-         if(i==n-1)
-            break;
-        for(int j=n-1; j>=0; j--)
-        {
-            if(j<=i)
-                cout<<"*";
-            else
-                cout<<" ";
-        }
+        for(int j=1; j<=n; j++)
+        cout<<j<<" ";
         cout<<endl;
     }
 
+
+    cout<<"\n2nd Pattern - L Number Pattern - 1\n";
+    cout<<endl;
+
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4 
+// 1 2 3 4 5
+
     for(int i=0; i<n; i++)
     {
-        for(int j=0; j<n; j++)
+        for(int j=0; j<=i; j++)
         {
-            if(j<i)
-                cout<<" ";
-            else
-                cout<<"*";
+            cout<<j+1<<" ";
         }
         cout<<endl;
     }
 
 
-    
+    cout<<"\n3rd Pattern - L Number Pattern - 2\n";
+    cout<<endl;
+
+// 1
+// 2 2
+// 3 3 3
+// 4 4 4 4
+// 5 5 5 5 5
+
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<=i; j++)
+        {
+            cout<<i+1<<" ";
+        }
+        cout<<endl;
+    }
 
 
+    cout<<"\n4th Pattern - Reverse L Number Pattern\n";
+    cout<<endl;
+
+// 1
+// 2 1
+// 3 2 1
+// 4 3 2 1
+// 5 4 3 2 1 
+
+    for(int i=0; i<n; i++)
+    {
+        for(int j=i; j>=0; j--)
+            cout<<j+1<<" ";
+        cout<<endl;
+    }
+
+
+
+    cout<<"\n5th Pattern - Reverse L Pattern in Numbers\n";
+    cout<<endl;
+
+// 1 2 3 4 5
+// 1 2 3 4
+// 1 2 3
+// 1 2
+// 1
+
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n-i; j++)
+        {
+            cout<<j+1<<" ";
+        }
+        cout<<endl;
+    }
+
+
+    cout<<"\n6th Pattern - L Pattern in Reverse\n";
+    cout<<endl;
+
+// 5
+// 5 4
+// 5 4 3
+// 5 4 3 2
+// 5 4 3 2 1
+
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<=i; j++)
+            cout<<n-j<<" ";
+        cout<<endl;
+    }
+
+
+    cout<<"\n7th Pattern - Mirror Image of L - 1\n";
+    cout<<endl;
+
+//     1
+//    22
+//   333
+//  4444
+// 55555
+
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n-i-1; j++)
+            cout<<" ";
+
+        for(int k=n-i-1; k<n; k++)
+            cout<<i+1;
+        cout<<endl;
+    }
+
+
+    cout<<"\n8th Pattern - Mirror Image of L - 2\n";
+    cout<<endl;
+
+//     1
+//    12
+//   123
+//  1234
+// 12345
+
+    for(int i=0; i<n; i++)
+    {
+        int num = 1;
+        for(int j=0; j<n-i-1; j++)
+            cout<<" ";
+
+        for(int k=n-i-1; k<n; k++)
+        {
+            cout<<num;
+            num++;
+        }
+        cout<<endl;
+    }
+
+
+    cout<<"\n9th Pattern - Mirror Image of L - 3\n";
+    cout<<endl;
+
+//     1
+//    21
+//   321
+//  4321
+// 54321
+
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n-i-1; j++)
+            cout<<" ";
+
+        for(int k=n-i-1; k<n; k++)
+        {
+            cout<<n-k;
+        }
+        cout<<endl;
+    }
+
+    cout<<"\n10th Pattern - Pyramid Pattern\n";
+    cout<<endl;
+
+//     1
+//    121
+//   12321
+//  1234321
+// 123454321
+
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n-i-1; j++)
+            cout<<" ";
+
+        for(int k=0; k<=i; k++)
+        {
+            cout<<k+1;
+        }
+
+        for(int l=i; l>=1; l--)
+        {
+            cout<<l;
+        }
+        cout<<endl;
+    }
 }
