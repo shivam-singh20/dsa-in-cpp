71a3d0d191f157fdb38031b513248ace784647c0
shivam-singh20 <shivamks5689@gmail.com>
2026-02-27 16:07:27 +0530

    Add code

--- Functions/fibonacci.cpp
+++ Functions/fibonacci.cpp
@@ -2,9 +2,22 @@
 #include <vector>
 using namespace std;
 
-vector<int>
-
 int main ()
 {
+    int n; 
+    cout<<"Enter range: ";
+    cin>>n;
 
+    vector<int>ans(n);
+
+    ans[0] = 0;
+    ans[1] = 1;
+
+    for(int i=2; i<n; i++)
+    {
+        ans[i] = ans[i-1] + ans[i-2];
+    }
+    cout<<"Fibonacci Series is as follow: "<<endl;
+    for(int i=0; i<n; i++)
+        cout<<ans[i]<<" ";
 }
--- Functions/practice.cpp
+++ Functions/practice.cpp
@@ -3,14 +3,5 @@ using namespace std;
 
 int main()
 {
-    int n; 
-    cin>>n;
-    char s = 'A';
     
-    for(int i=0; i<n; i++)
-    {
-        for(int j=0; j<=i; j++)
-            cout<<j+i+1;
-        cout<<endl;
-    }
 }
