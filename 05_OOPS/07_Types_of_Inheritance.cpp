352cc7294d0f01803107c276e527e5b74c652b57
shivam-singh20 <shivamks5689@gmail.com>
2026-04-20 15:02:05 +0530

    All types of inheritance is done.

--- 05_OOPS/07_Types_of_Inheritance.cpp
+++ 05_OOPS/07_Types_of_Inheritance.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
 
-// Types of Inheritance - Single, Multilevel, Multiple, Hierarchical, 
+// Types of Inheritance - Single, Multilevel, Multiple, Hierarchical, Hybrid
 
-// Single Inheritance
+// Single Inheritance - Simple Inheritance
 class Vehical {
     protected:
     int tyres;
@@ -21,7 +21,11 @@ class Car: public Vehical {
     }
 };
 
-// Multilevel Inheritance
+// Multilevel Inheritance - One derived class inherits properties from another base class and another derived 
+//                          class inherits properties from 1st derived class as Base class.
+
+// For example - Son inherits properties from his father and father inherits properties from grandfather.
+
 class Person {
     protected:
     string name;
@@ -69,8 +73,89 @@ class Manager: public Employee { //Inherits property from Person, Employee class
     }
 };
 
-// Multiple Inheritance
+// Multiple Inheritance - One single Derived class inherits properties from multiple base class.
 
+class Gaming { // Base class
+    public:
+    void display()
+    {
+        cout<<"I am a Gamer."<<endl;
+    }
+};
+
+class Teaching { // Base class
+    public:
+        void display()
+        {
+            cout<<"I am a Teacher."<<endl;
+        }
+};
+
+class Content_Creation { // Base class
+    public: 
+        void display()
+        {
+            cout<<"I am a content creator."<<endl;
+        }
+};
+
+
+class Youtube : public Gaming, public Teaching, public Content_Creation {
+    public:
+        void display()
+        {
+            cout<<"I am a Youtuber.";
+        }
+};
+
+
+// Hierarchical Inheritance - Multiple derived class inherits properties from single base class.
+
+class Human {
+    protected:
+        string name;
+        int age;
+        float height;
+};
+
+class Student : public Human {
+    int student_id, roll_no;
+
+    public:
+        Student(int student_id, string name, int age, int roll_no, float height)
+        {
+            this->student_id = student_id;
+            this->name = name;
+            this->age = age;
+            this->roll_no = roll_no;
+            this->height = height;
+        }
+
+        void get_info()
+        {
+            cout<<"Student_id : "<< student_id <<" Name : "<< name <<" Age : "<<age<<" Roll_no : "<<roll_no<<" Height : "<< height<<endl;
+        }
+};
+
+class Teacher : public Human {
+    int teacher_id;
+    double salary;
+
+    public:
+        Teacher(int teacher_id, string name, int age, int salary, float height)
+        {
+            this->teacher_id = teacher_id;
+            this->name = name;
+            this->age = age;
+            this->salary = salary;
+            this->height = height;
+        }
+
+        void get_info()
+        {
+            cout<<"Teacher_id : "<< teacher_id <<" Name : "<< name <<" Age : "<<age<<" Salary : "<<salary<<" Height : "<< height<<endl;
+        }
+};
 
 int main()
 {
